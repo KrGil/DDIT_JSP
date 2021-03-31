539cf54f5416e684f5ba6118ed6f9b8f016b172b
KrGil <kanjjang89@naver.com>
2021-03-31 21:18:50 +0900

    openssl, login_mypageForm_mypage

---
+++ webStudy01/src/kr/or/ddit/member/controller/MemberInsertServlet.java
@@ -0,0 +1,126 @@
+package kr.or.ddit.member.controller;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import kr.or.ddit.enumpkg.ServiceResult;
+import kr.or.ddit.service.IMemberService;
+import kr.or.ddit.service.MemberServiceImpl;
+import kr.or.ddit.vo.MemberVO;
+
+@WebServlet("/member/memberInsert.do")
+public class MemberInsertServlet extends HttpServlet {
+	private IMemberService service = new MemberServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String view = "/WEB-INF/views/member/memberForm.jsp";
+		boolean redirect = false;
+		// logic
+		if (redirect) {
+			// redirect는 클라이언트 사이드의 경로 302로 보내주어야하니
+			resp.sendRedirect(req.getContextPath() + view);
+		} else {
+			req.getRequestDispatcher(view).forward(req, resp);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+//		1. 요청 접수
+		MemberVO member = new MemberVO();
+		
+		req.setAttribute("member", member);
+		// 규칙성 Mem_id와 변수명 mem_id가 같다. 그럼 reflection을 쓸 수 있다.
+//		member.setMem_id(req.getParameter("mem_id"));
+		try {
+			BeanUtils.populate(member, req.getParameterMap());
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+		
+//		2. 검증( 데이터의 목적, 경로에 따라 방법이 달라져야 한다.)
+		Map<String, String> errors = new LinkedHashMap<String, String>();
+		req.setAttribute("errors", errors);
+
+		boolean valid = validate(member, errors);
+		boolean redirect = false;
+		String view = null;
+		String message = null;
+
+		if (valid) {
+			ServiceResult result = service.createMember(member);
+			switch (result) {
+			case PKDUPLICATED:
+				view = "/WEB-INF/views/member/memberForm.jsp";
+				message = "아이디 중복";
+				break;
+			case OK:
+				redirect = true;
+				view = "/login/loginForm.jsp";
+				break;
+			default:
+				message = "서버 오류, 잠시 후 다시 시도해주세요.";
+				view = "/WEB-INF/views/member/memberForm.jsp";
+				break;
+			}
+		} else {
+			// 검증 불통
+			view = "/WEB-INF/views/member/memberForm.jsp";
+		}
+
+		req.setAttribute("message", message);
+
+		if (redirect) {
+			// redirect는 클라이언트 사이드의 경로 302로 보내주어야하니
+			resp.sendRedirect(req.getContextPath() + view);
+		} else {
+			req.getRequestDispatcher(view).forward(req, resp);
+		}
+
+	}
+
+	private boolean validate(MemberVO member, Map<String, String> errors) {
+		boolean valid = true;
+		if (member.getMem_id() == null || member.getMem_id().isEmpty()) {
+			valid = false;
+			errors.put("mem_id", "회원이름 누락");
+		}
+		if (member.getMem_pass() == null || member.getMem_pass().isEmpty()) {
+			valid = false;
+			errors.put("mem_pass", "비밀번호 누락");
+		}
+		if (member.getMem_name() == null || member.getMem_name().isEmpty()) {
+			valid = false;
+			errors.put("mem_name", "이름 누락");
+		}
+		if (member.getMem_zip() == null || member.getMem_zip().isEmpty()) {
+			valid = false;
+			errors.put("mem_zip", "우편번호 누락");
+		}
+		if (member.getMem_add1() == null || member.getMem_add1().isEmpty()) {
+			valid = false;
+			errors.put("mem_add1", "주소1 누락");
+		}
+		if (member.getMem_add2() == null || member.getMem_add2().isEmpty()) {
+			valid = false;
+			errors.put("mem_add2", "주소2 누락");
+		}
+		if (member.getMem_mail() == null || member.getMem_mail().isEmpty()) {
+			valid = false;
+			errors.put("mem_mail", "이메일 누락");
+		}
+		return valid;
+	}
+}
--- webStudy01/src/kr/or/ddit/service/MemberServiceImpl.java
+++ webStudy01/src/kr/or/ddit/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.service;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 import org.apache.commons.beanutils.BeanUtils;
 
@@ -19,5 +20,53 @@ public class MemberServiceImpl implements IMemberService {
 		}
 		return savedMember;
 		// 특정 상황에 쓸 수 있는 커스텀 익셉션
+	}
+//	@Override
+//	public ServiceResult createMember(MemberVO member) {
+//		int cnt =  dao.insertMember(member);
+//		MemberVO savedMember = dao.selectMemberDetail(member.getMem_id());
+//		System.out.println("createMember_savedMember : "+savedMember);
+//		ServiceResult result = null;
+//		if(cnt==0){ // 실패
+//			if(savedMember != null) { // 조회 후 값이 존재한다면
+//				result = ServiceResult.PKDUPLICATED;
+//			}else {
+//				result = ServiceResult.FAIL;
+//			}
+//		}else { // 성공
+//			result = ServiceResult.OK;
+//		}
+//		return result;
+//	}
+	@Override
+	public ServiceResult createMember(MemberVO member) {
+		ServiceResult result = null;
+		if(dao.selectMemberDetail(member.getMem_id())==null) { // 중복되지 않는다면
+			int rowcnt = dao.insertMember(member);
+			if(rowcnt>0) { // 성공
+				result = ServiceResult.OK;
+			}else { // 실패
+				result = ServiceResult.FAIL;
+			}
+		}else { // 중복
+			result = ServiceResult.PKDUPLICATED;
+		}
+		return result;
+	}
+	
+	@Override
+	public ServiceResult modifyMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public ServiceResult removeMember(MemberVO member) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<MemberVO> retrieveMemberList() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
